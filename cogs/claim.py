2ff37fcabdd566a3c2d0059f0d1b23621f6ab6f3
Abdullah Baig <rebelschaos@gmail.com>
2021-11-25 22:16:29 +0500

    first commit

---
+++ cogs/claim.py
@@ -0,0 +1,35 @@
+import json
+import os
+import sys
+from random import randint
+
+import discord
+from discord.ext import commands
+
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
+
+
+def has_roles(context):
+    roles = [role.name for role in context.message.author.roles]
+    if "Admin" in roles:
+        return True
+    return False
+
+
+class Claim(commands.Cog, name="claim"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="join", description=f"Join collab.land")
+    async def join(self, context):
+        embed = discord.Embed(color=randint(0, 0x000ff), description="**Connect Your Account\n**"
+                                                                     f"To connect your wallet/account please [Click here]({config['redirect_link']}).")
+        await context.author.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Claim(bot))
